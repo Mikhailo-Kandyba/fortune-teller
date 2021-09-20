60f2286f6d49fc26d4f0467b5bc1aea6351879ca
Mikhailo-Kandyba <mikhailo.kandyba0gmail.com>
2021-09-20 18:23:44 +0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,5 @@ $(".btn-menu").on('click', function(e) {
     e.preventDefault();
     $(this).toggleClass("active");
     $(".mobile-menu-navigation").toggleClass('show-menu');
+    $("body").toggleClass('overflow-hidden');
 });
