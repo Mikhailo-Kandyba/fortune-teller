28ee28f7839340c3c0a2936ce20308a11ed9d63f
Mikhailo-Kandyba <mikhailo.kandyba0gmail.com>
2021-09-21 18:57:26 +0300

    menu-navigation-wrap-update

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
 
+// $(".btn-menu").on('click', function(e) {
+//     e.preventDefault();
+//     $(this).toggleClass("active");
+//     $(".mobile-menu-navigation").toggleClass('show-menu');
+//     $("body").toggleClass('overflow-hidden');
+// });
+
+
 $(".btn-menu").on('click', function(e) {
     e.preventDefault();
     $(this).toggleClass("active");
-    $(".mobile-menu-navigation").toggleClass('show-menu');
+    $(".menu-navigation-wrap").toggleClass('show-menu');
     $("body").toggleClass('overflow-hidden');
 });
